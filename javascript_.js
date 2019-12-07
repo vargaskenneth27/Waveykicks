c63f51277f92c12fb3018d47e83a546046a69370
vargaskenneth27 <57016938+vargaskenneth27@users.noreply.github.com>
2019-12-07 17:16:34 -0500

    Add files via upload

---
+++ javascript_.js
@@ -0,0 +1,12 @@
+// This code below was for passsword request page (pas_for.html) but needed online storage for it to work//
+
+
+// @{
+// newPassword = Request["newPassword"];
+// confirmPassword = Request["confirmPassword"];
+// token = Request["token"];
+// if IsPost
+// {
+//     // input testing is ommitted here to save space
+//     retunValue = ResetPassword(token, newPassword);
+// }//
